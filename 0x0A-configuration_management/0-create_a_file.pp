4aa74378497305cd559e462e136cb6e2388f1f72
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-01-20 04:42:58 +0000

    Hot fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,7 +4,7 @@ file { '/tmp/school':
   ensure  => file,
   path    => '/tmp/school',
   mode    => '0744',
-  owner   => 'www-data'
+  owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
 }
