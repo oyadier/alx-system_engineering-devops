7cc64a13a13f02d36b5f333cee7b1d0002623864
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-01-20 04:31:42 +0000

    formatting

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Creating file in puppet
 
-file {'/tmp/school':
-ensure => 'file'
-path => '/tmp/school',
-mode => '0744',
-owner => 'www-data'
-group => 'www-data',
-content: 'I love Puppet',
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data'
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
