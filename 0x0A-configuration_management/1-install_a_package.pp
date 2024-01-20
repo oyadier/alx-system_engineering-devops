3e097921adb1f091fa154c6232edf978aabfdf23
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-01-20 04:51:06 +0000

    Hot fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# declarative scrip for installing packeg using puppet
+# declarative scrip for installing packeg using
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+/* puppet declarative script to grab puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
+}*/
