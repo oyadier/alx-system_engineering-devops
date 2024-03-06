009f4b221c7010bdd4d009df48e882d515a49539
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-03-06 05:33:21 +0000

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python3
-"""Retreiving Post"""
+""" Top ten """
 
-
-import requests
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'Custom User Agent'}  # Set a custom User-Agent header
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit
+    """
+    from requests import get
 
-    if response.status_code == 200:
-        data = response.json()
-        posts = data.get('data', {}).get('children', [])
-        for post in posts:
-            print(post['data']['title'])
-    else:
-        print("None")
+    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
 
+    headers = {'user-agent': 'my-app/0.0.1'}
+
+    r = get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        print(None)
+        return None
+
+    try:
+        js = r.json()
+
+    except ValueError:
+        print(None)
+        return None
+
+    try:
+
+        data = js.get("data")
+        children = data.get("children")
+        for child in children[:10]:
+            post = child.get("data")
+            print(post.get("title"))
+
+    except Exception:
+        print(None)
