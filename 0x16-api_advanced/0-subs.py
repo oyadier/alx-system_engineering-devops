79be6ca44b14084e23f16a7023e0558d3ae04c44
Oyadier Robert Mensah <52941396+oyadier@users.noreply.github.com>
2024-03-06 05:54:56 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,9 @@ from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """ecursive function that queries the Reddit API and returns a list
-    containing the titles of all hot articles for a given subreddit. If no
-    results are found for the given subreddit, the function should return None
+    """ 
+        If no results are found for the given subreddit, the function
+        should return None
     """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
