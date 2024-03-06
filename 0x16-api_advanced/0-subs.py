8105f6afbf80493b665c18eb2c37a60e85991915
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-03-06 04:35:30 +0000

    Fist commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Function to Get subscriber from Reddit'''
+
+
+def number_of_subscribers(subreddit):
+    '''
+        Args:
+            subreddit (string): the api endpoint
+            Returns (Int): the total number of subreddit
+    '''
+    from requests import get
+    headers = {'user-agent': 'advance-api/1.0.0'}
+    sub_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    subscribe = get(sub_url, headers=headers, allow_redirects=False)
+    if subscribe.status_code == 200:
+        count = subscribe.json()
+        return count['data']['subscribers']
+    else:
+        return 0
