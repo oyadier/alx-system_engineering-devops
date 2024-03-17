15e50ef783e6d5bdacf58e698478cde3614b5b28
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-03-17 03:31:18 +0000

    Increase FD

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 # Fix the nginx requests limit
 exec { 'upgrade':
   path    => '/bin/',
-  command => 'sed -i "s/15/4096" /etc/default/nginx',
+  command => 'sudo sed -i "s/15/4096" /etc/default/nginx',
 }
 
 exec { 'restart':
   path    => '/usr/bin/',
-  command => 'service nginx restart',
+  command => 'sudo service nginx restart',
 }
