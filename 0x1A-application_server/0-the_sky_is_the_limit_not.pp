152e0aca02927872784e8ca5ac965777068a7093
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-03-19 09:53:26 +0000

    Serving odd number

--- 0x1A-application_server/0-the_sky_is_the_limit_not.pp
+++
@@ -1,9 +0,0 @@
-exec { 'increase-fd':
-  path => '/bin/'
-  command => 'sed -i "s/15/1400" /etc/default/nginx',
-}
-exec { 'restart':
-  path => '/bin/',
-  command => 'service nginx restart',
-}
-	
